91de60665ac1a571a5b6cd1373440b8c8f5f4dff
Julius Dsouza <jsdsz1996@gmail.com>
2021-06-23 19:57:58 -0400

    GoalItem Component Created

--- App.js
+++ App.js
@@ -1,44 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
-import GoalItem from './components/GoalItem'
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
   // const [outputText, setOutputText] = useState('Not Pressed Button');
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]); //Empty Array has been passed as initial value
 
   // function goalInputHandler(enteredText){
   //   setEnteredGoal(enteredText);
   // }
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
+  const addGoalHandler = (goalTitle) => {
     // console.log(enteredGoal);
     // setCourseGoals([...courseGoals, enteredGoal]); // Should be a snapshot of the current state but not 100% guarantee
     // setCourseGoals(currentGoals => [...courseGoals, enteredGoal]); 
     setCourseGoals(currentGoals => [
       ...courseGoals, 
-      {id: Math.random().toString(), value: enteredGoal}
+      {id: Math.random().toString(), value: goalTitle}
     ]); // Supports Key and Id
   };
 
   return (
-    // <View style={styles.container}>
-    //   <Text>{outputText}</Text>
-    //   <Button title="Press" onPress={() => setOutputText('PRESSED')}/>
-    //   <StatusBar style="auto" />
-    // </View>
-
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-        {/* <Button title="ADD" onPress={() => setOutputText('PRESSED')} /> */}
-        <Button title="ADD" onPress={addGoalHandler} /> 
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       {/* <ScrollView >
         {courseGoals.map((goal) => <View style={styles.listItem} key={goal}><Text>{goal}</Text></View>)}
       </ScrollView> */}
@@ -51,12 +37,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
   screen: {
     padding: 60,
   },
